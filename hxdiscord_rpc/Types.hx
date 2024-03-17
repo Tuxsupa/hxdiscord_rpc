4d3c9b1616c4215cc943e4c8dc4a29b2a856ae35
Tuxsuper <69093264+Tuxsupa@users.noreply.github.com>
2024-03-17 16:47:59 +0000

    Add buttons to Types

--- hxdiscord_rpc/Types.hx
+++ hxdiscord_rpc/Types.hx
@@ -31,6 +31,10 @@ extern class DiscordRichPresence
 	var matchSecret:cpp.ConstCharStar; /* max 128 bytes */
 	var joinSecret:cpp.ConstCharStar; /* max 128 bytes */
 	var spectateSecret:cpp.ConstCharStar; /* max 128 bytes */
+	var button1Label:cpp.ConstCharStar; /* max 32 bytes */
+    var button1Url:cpp.ConstCharStar;   /* max 512 bytes */
+    var button2Label:cpp.ConstCharStar; /* max 32 bytes */
+    var button2Url:cpp.ConstCharStar;   /* max 512 bytes */
 	var instance:cpp.Int8;
 }
 
